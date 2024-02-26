ed4a1d0a10637c822da1f49d03d49d2eac072952
Sebastian-123-web <sebastian09_26@hotmail.com>
2024-02-26 02:27:12 -0500

    RB | Se mejoro la funcion para actualizar contraseña

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -3,47 +3,61 @@ const bcrypt = require('bcrypt')
 
 
 /*
-*****************************************
+***********************************************
         ACTUALIZAR CONTRASEÑA
-*****************************************
+***********************************************
 */
-const updatePassword = (req, res) => {
+const updatePassword = async (req, res) => {
     const { password,id_user } = req.body
-    console.log('1')
-    const passwordEncrypt = encryptPassword(password)
-    console.log('3')
-    console.log(passwordEncrypt)
-    if(savePasswordInDB(id_user,passwordEncrypt)){
-        res.status(200).json({msg:'Contraseña Actualizada'})
+    if(!await encryptPassword(id_user,password,savePasswordInDB)){
+        return res.json({msg:'No se actualizo'})
     }
-    res.json({msg:'No se actualizo'})
-}
-
-// ENCRIPTAMOS LA CONTRASEÑA
-const encryptPassword = (password) => {
-    bcrypt.hash(password,10, (err, hashedPassword)=>{
-        if(err) throw err
-        //console.log(hashedPassword)
-        console.log('2')
-        return hashedPassword
-    })
+    res.status(200).json({msg:'Contraseña Actualizada'})
 }
 
 // ACTUALIZA LA CONTRASEÑA ENCRIPTADA EN LA BASE DE DATOS
 const savePasswordInDB = (id_user,password) => {
-    const querySQL = `UPDATE tbl_password SET password = '${password}' WHERE tbl_password.id_user = ${id_user}`
-    //console.log(querySQL)
-    connection.query(querySQL, (err, result)=>{
-        if(err) throw err
-        return true
-    })
+    try{
+        return new Promise((resolve,reject)=>{
+            const querySQL = `UPDATE tbl_password SET password = '${password}' WHERE tbl_password.id_user = ${id_user}`
+            connection.query(querySQL, (err, result)=>{
+                if(err) reject(err)
+                //console.log("salio true")
+                resolve(true)
+            })
+        })
+    }catch(err){
+        console.error("Error: ",err)
+        throw err
+    }
 }
 
 
 /*
-*****************************************
-        REGISTRO DE USUARIO
-*****************************************
+***********************************************
+    ENCRIPTAR CONTRASEÑA ---> REUTILIZABLE
+***********************************************
+*/
+const encryptPassword = (id_user,password,sqlFunction) => {
+    try {
+        return new Promise((resolve, reject)=>{
+            bcrypt.hash(password,10, (err, hashedPassword)=>{
+                if(err) reject(err)
+                resolve(sqlFunction(id_user,hashedPassword))
+            })
+        })
+    } catch (error) {
+        console.log("Error: ", error)
+        throw error
+    }
+}
+
+
+
+/*
+***********************************************
+            REGISTRO DE USUARIO
+***********************************************
 */
 
 const userRegister = (req,res) => {
@@ -63,9 +77,9 @@ const getIdUser = (user_email) => {
     })
 }
 
-const userPasswordRegister = () => {
+const userPasswordRegister = (password) => {
     const id_user = getIdUser
-    const querySQL = `INSERT INTO tbl_password (id_user, password) VALUES (${id_user}, '')`
+    const querySQL = `INSERT INTO tbl_password (id_user, password) VALUES (${id_user}, '${password}')`
     connection.query(querySQL, (err, result) => {
         if(err) throw result
         
