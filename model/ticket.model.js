b944cb024d34a26cce17c2dae03417cd0c8f3fc0
Sebastian-123-web <sebastian09_26@hotmail.com>
2024-04-12 00:42:37 -0500

    RB | se esta creando la clase ticket

--- model/ticket.model.js
+++ model/ticket.model.js
@@ -19,5 +19,18 @@ nuevo ticket
 const connection = require('../config/database_config')
 
 class Ticket {
-    
+    constructor(ticket_status,ticket_title,ticket_description,ticket_important,ticket_anydesk,ticket_device,ticket_creationdate,ticket_closedate) {
+        this.ticket_status = ticket_status
+        this.ticket_title = ticket_title
+        this.ticket_description = ticket_description
+        this.ticket_important = ticket_important
+        this.ticket_anydesk = ticket_anydesk
+        this.ticket_device = ticket_device
+        this.ticket_creationdate = ticket_creationdate
+        this.ticket_closedate = ticket_closedate
+    }
+
+    static getAllTicket(){
+        
+    }
 }
